721f7dee3632689b5c40ef7117accb7d7a3559bb
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-15 17:36:56 -0600

    complete course 53. Use Assertion Library to make test easier

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,10 @@
 module.exports.add = (a,b)=> a+b;
 
 module.exports.square = (a)=> a*a;
+
+module.exports.name = (user, fullname)=>{
+    var name = fullname.split(' ');
+    user.firstname = name[0];
+    user.lastname = name[1];
+    return user;
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,32 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 //it is provided by Mocha
 it('should add two numbers', ()=>{
     var res = utils.add(33,11);   
     
-    if(res != 44){
-        throw new Error(`Expected 44, but get ${res}`);
-    };
+    //use expect to do the test.
+    //.toBe(); .toBeA();
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square the input number', ()=>{
     var square = utils.square(4);
 
-    if(square !==16){
-        throw new Error(`Expected 16, but get ${square}`);
-    };
+    expect(square).toBeA('number').toBe(16);
 })
+
+it('should set first and last name', ()=>{
+    var user = {
+        location : 'Toronto',
+        age: 23
+    };
+    var res = utils.name(user, 'Mark Qian');
+    expect(res).toInclude({
+        firstname: 'Mark',
+        lastname:'Qian'
+    });
+    //expect(user).toEqual(res);
+
+});
